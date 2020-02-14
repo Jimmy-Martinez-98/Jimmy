0c5e07d2a741a1543362fc8c20a18d4e1794aca3
immy-Martinez-98 <usis000718@ugb.edu.sv>
2020-02-13 21:14:09 -0600

    tarea

---
+++ app.js
@@ -0,0 +1,58 @@
+
+document.addEventListener("DOMContentLoaded", (event) =>{
+    const formAlumnos = document.querySelector("#frmAlumno");
+    formAlumnos.addEventListener("submit", (e)=>{
+        e.preventDefault(); 
+        let codigo = document.querySelector("#txtCodigoAlumno").value,
+            nombre = document.querySelector("#txtNombreAlumno").value,
+            direccion = document.querySelector("#txtDireccionAlumno").value,
+            telefono = document.querySelector("#txtTelefonoAlumno").value;
+
+            var keycodigo="codigo"+" "+nombre;
+            var keynombre="nombre"+" "+nombre;
+            var keydireccion="direccion"+" "+nombre;
+            var keytelefono="telefono"+" "+nombre;
+
+
+        if( 'localStorage' in window ){
+            window.localStorage.setItem(keycodigo, codigo);
+            window.localStorage.setItem(keynombre, nombre);
+            window.localStorage.setItem(keydireccion, direccion);
+            window.localStorage.setItem(keytelefono, telefono);
+            document.querySelector("#txtCodigoAlumno").value="";
+            document.querySelector("#txtNombreAlumno").value="";
+            document.querySelector("#txtDireccionAlumno").value="";
+            document.querySelector("#txtTelefonoAlumno").value="";
+            
+
+        } else {
+            alert("almacenamiento en local NO soportado!!! Actualizate!");
+        }
+        console.log(codigo,nombre,direccion,telefono);
+        
+    });
+    document.querySelector("#btnRecuperarAlumnos").addEventListener("click",(e)=>{
+        if( 'localStorage' in window ){
+            document.querySelector("#txtCodigoAlumno").value = 
+                window.localStorage.getItem("codigo");
+            document.querySelector("#txtNombreAlumno").value = 
+                window.localStorage.getItem("nombre");
+            document.querySelector("#txtDireccionAlumno").value = 
+                window.localStorage.getItem("direccion");
+            document.querySelector("#txtTelefonoAlumno").value = 
+                window.localStorage.getItem("telefono");
+        }  else {
+            alert("almacenamiento en local NO soportado!!! Actualizate!");
+        }
+    });
+});
+
+/*document.addEventListener("DOMContentLoaded",init);*/
+
+/*document.addEventListener("DOMContentLoaded",function(event){
+    alert("Pagina cargo forma 2");
+});*/
+
+/*function init(event){
+    alert("Hola la pagina a cargado");
+}*/
