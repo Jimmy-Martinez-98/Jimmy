ed0d8087b423011ac2a81492874fc1ea50596c55
immy-Martinez-98 <usis000718@ugb.edu.sv>
2020-03-02 15:52:44 -0600

    j

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ document.addEventListener("DOMContentLoaded", e=>{
   const form = document.querySelector("#frmSaludo");
   form.addEventListener("submit", event=>{
       event.preventDefault();
-      
-
+      mediana();
   });
-function moda(){
-  let valores = document.querySelector("#txtDatos").nodeValue;
-}
-
 });
+
+function mediana() {
+  let vlor = document.querySelector("#txtDatos").value, dtos = vlor.split(',').sort(), suma1 = 0;
+  for (i = 0; i < dtos.length; i++) {
+    dtos[i] = parseInt(dtos[i]);
+  }
+  if (dtos.length % 2 == 0) {
+    suma1 = (dtos[Math.round((dtos.length / 2) - 1)] + dtos[Math.round(dtos.length / 2)]) / 2;
+    document.querySelector("#lblSaludo").innerHTML = suma1;
+  }
+  else {
+    suma1 = dtos[Math.round((dtos.length / 2) - 1)];
+    document.querySelector("#lblSaludo").innerHTML = suma1;
+  }
+}
