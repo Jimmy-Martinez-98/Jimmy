05231c376fe72523c69b9f89638abe55b6014eb3
immy-Martinez-98 <usis000718@ugb.edu.sv>
2020-03-01 21:15:21 -0600

    estadistica

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ document.addEventListener("DOMContentLoaded", e=>{
   const form = document.querySelector("#frmSaludo");
   form.addEventListener("submit", event=>{
       event.preventDefault();
+      
 
-     let nombre = document.querySelector("#txtNombre").value;
-     fetch(`saludo.php?name=${nombre}`)
-          .then(resp=>resp.text())
-          .then(respuesta=>{
-              document.querySelector("#lblSaludo").innerHTML = respuesta;
-          });
   });
+function moda(){
+  let datos = document.querySelector("#txtDatos")
+}
+
 });
