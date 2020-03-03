645fbe3e8f50dbe70ecc41c553f5cb657797edcd
immy-Martinez-98 <usis000718@ugb.edu.sv>
2020-03-02 20:15:23 -0600

    estadistica

--- app.js
+++ app.js
@@ -2,21 +2,28 @@ document.addEventListener("DOMContentLoaded", e=>{
   const form = document.querySelector("#frmSaludo");
   form.addEventListener("submit", event=>{
       event.preventDefault();
-      mediana();
+      moda();
   });
 });
 
 function mediana() {
-  let vlor = document.querySelector("#txtDatos").value, dtos = vlor.split(',').sort(), suma1 = 0;
+  let vlor = document.querySelector("#txtDatos").value, dtos = vlor.split(',').sort(), mediana = 0;
   for (i = 0; i < dtos.length; i++) {
     dtos[i] = parseInt(dtos[i]);
   }
   if (dtos.length % 2 == 0) {
-    suma1 = (dtos[Math.round((dtos.length / 2) - 1)] + dtos[Math.round(dtos.length / 2)]) / 2;
-    document.querySelector("#lblSaludo").innerHTML = suma1;
+    mediana = (dtos[Math.round((dtos.length / 2) - 1)] + dtos[Math.round(dtos.length / 2)]) / 2;
+    document.querySelector("#lblSaludo").innerHTML = mediana;
   }
   else {
-    suma1 = dtos[Math.round((dtos.length / 2) - 1)];
-    document.querySelector("#lblSaludo").innerHTML = suma1;
+    mediana = dtos[Math.round((dtos.length / 2) - 1)];
+    document.querySelector("#lblSaludo").innerHTML = mediana;
+  }
+}
+function moda(){
+  let vlor = document.querySelector("#txtDatos").value, dtos = vlor.split(',').sort(), mayor = 0, frc =vlor.split(',').sort() ,moda=0;
+  for (i = 0; i < dtos.length; i++) {
+    dtos[i] = parseInt(dtos[i]);
+    frc[i]=0;
   }
 }
