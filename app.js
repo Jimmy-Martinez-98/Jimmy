eaae8098a2f4b899fd2d6807965660622ff14f40
immy-Martinez-98 <usis000718@ugb.edu.sv>
2020-03-03 11:06:27 -0600

    estadistica

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ document.addEventListener("DOMContentLoaded", e=>{
   const form = document.querySelector("#frmSaludo");
   form.addEventListener("submit", event=>{
       event.preventDefault();
-      moda();
+      let vlor = document.querySelector("#txtDatos").value, dtos = vlor.split(',').sort()
+      Moda(dtos);
   });
 });
-
+//encontrar-mediana
 function mediana() {
-  let vlor = document.querySelector("#txtDatos").value, dtos = vlor.split(',').sort(), mediana = 0;
+  let vlor = document.querySelector("#txtDatos").value, dtos = vlor.split(',').sort(compararnum), mediana = 0;
   for (i = 0; i < dtos.length; i++) {
     dtos[i] = parseInt(dtos[i]);
   }
@@ -17,13 +18,49 @@ function mediana() {
   }
   else {
     mediana = dtos[Math.round((dtos.length / 2) - 1)];
-    document.querySelector("#lblSaludo").innerHTML = mediana;
+    document.querySelector("#lblSaludo").innerHTML = `la mediana es: ${(mediana)}`;
   }
 }
-function moda(){
-  let vlor = document.querySelector("#txtDatos").value, dtos = vlor.split(',').sort(), mayor = 0, frc =vlor.split(',').sort() ,moda=0;
+//encontrar moda
+function Moda(serie=[]){
+  let datos=document.getElementById("txtDatos").value;
+  let array=datos.split(",").sort(compararnum);
+  let repetidos = [];
+ let temporal = [];
+  
+  array.forEach((value,index)=>{
+    temporal = Object.assign([],array); //Copiado de elemento
+    temporal.splice(index,1); //Se elimina el elemnto q se compara
+    /**
+     * Se busca en temporal el elemento, y en repetido para 
+     * ver si esta ingresado al array. indexOf returna
+     * -1 si el elemento no se encuetra
+     **/
+    if(temporal.indexOf(value)!=-1 && repetidos.indexOf(value)==-1) {
+       repetidos.push(value);}  
+  });
+  let $resp=document.getElementById("lblSaludo")
+  $resp.innerHTML=`La Moda es:  ${(repetidos)}`;
+  console.log(repetidos);
+}
+//odenar-datos
+function compararnum(a, b) {
+  return a - b;
+}
+//encontrar-media
+function media(){
+  let vlor = document.querySelector("#txtDatos").value, dtos = vlor.split(',').sort(compararnum), media = 0;
   for (i = 0; i < dtos.length; i++) {
     dtos[i] = parseInt(dtos[i]);
-    frc[i]=0;
+    media = media + dtos[i]
   }
+  document.querySelector("#lblSaludo").innerHTML = `La media es: ${(media/dtos.length)}`;
+}
+//desviacion-tipica
+function desviacion_tipica(){
+let vlor = document.querySelector("#txtDatos").value, dtos = vlor.split(",").sort(compararnum), des_tip = 0;
+for(i=o; i < dtos.length;i++){
+  dtos[i] = parseInt(datos[i]);
+}
+
 }
